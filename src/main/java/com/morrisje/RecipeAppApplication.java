943ea83e60bbc3d3578c04449402ab8d69c02b83
John Morris <johnmorris4@bellsouth.net>
2018-12-13 03:29:10 -0500

    initial commit

---
+++ src/main/java/com/morrisje/RecipeAppApplication.java
@@ -0,0 +1,14 @@
+package com.morrisje;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecipeAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RecipeAppApplication.class, args);
+    }
+
+}
+
