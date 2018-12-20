7fb7e109583f39d6d9673f6dde31379319620f65
johnmorris4 <johnmorris@morrisje.com>
2018-12-20 00:41:40 -0500

    Completed setting up mapping between ingredients and unit of measure

--- src/main/java/com/morrisje/domain/Ingredient.java
+++ src/main/java/com/morrisje/domain/Ingredient.java
@@ -13,7 +13,7 @@ public class Ingredient {
     private String description;
     private BigDecimal amount;
 
-    @OneToMany
+    @OneToOne(fetch =FetchType.EAGER)
     private UnitOfMeasure uom;
 
 
