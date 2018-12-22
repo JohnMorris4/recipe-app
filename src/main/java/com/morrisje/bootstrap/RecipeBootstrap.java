d3a41e8a0f79d464d58b35d13866dd9f7d991c4d
John Morris <johnmorris@morrisje.com>
2018-12-22 01:28:38 -0500

    Added Bootstrap 3.3.7 to project. closes #1

--- src/main/java/com/morrisje/bootstrap/RecipeBootstrap.java
+++ src/main/java/com/morrisje/bootstrap/RecipeBootstrap.java
@@ -8,6 +8,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.ApplicationListener;
 import org.springframework.context.event.ContextRefreshedEvent;
 import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.math.BigDecimal;
 import java.util.ArrayList;
@@ -29,6 +30,7 @@ public class RecipeBootstrap implements ApplicationListener<ContextRefreshedEven
 
 
     @Override
+    @Transactional
     public void onApplicationEvent(ContextRefreshedEvent contextRefreshedEvent) {
         log.debug("Loading Bootstrapping of the project");
         recipeRepository.saveAll(getRecipies());
