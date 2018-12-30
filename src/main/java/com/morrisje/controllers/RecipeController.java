bbd97e5ba694c5152bde09e9d63fba341245c7b7
johnmorris4 <johnmorris@morrisje.com>
2018-12-30 05:35:59 -0500

    Added the Add or Update method to the controller. Added the recipeform

--- src/main/java/com/morrisje/controllers/RecipeController.java
+++ src/main/java/com/morrisje/controllers/RecipeController.java
@@ -1,10 +1,13 @@
 package com.morrisje.controllers;
 
 
+import com.morrisje.commands.RecipeCommand;
 import com.morrisje.services.RecipeService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -24,6 +27,20 @@ public class RecipeController {
 
         return "recipe/show";
 
+    }
+
+    @RequestMapping("/recipe/new")
+    public String newRecipe(Model model){
+        model.addAttribute("recipe", new RecipeCommand());
+        return "recipe/recipeform";
+    }
+
+    @PostMapping
+    @RequestMapping("recipe")
+    public String saveOrUpdate(@ModelAttribute RecipeCommand command){
+        RecipeCommand savedCommand = recipeService.saveRecipeCommand(command);
+
+        return "redirect:/recipe/show/" + savedCommand.getId();
     }
 
 }
