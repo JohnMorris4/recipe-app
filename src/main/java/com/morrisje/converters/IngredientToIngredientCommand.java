7b10e10fc24f06527cb397cdd2d5f3b9fbadb7b4
johnmorris4 <johnmorris@morrisje.com>
2019-01-14 05:44:31 -0500

    Fixed problem with NPE in IngredientToIngredientCommand

--- src/main/java/com/morrisje/converters/IngredientToIngredientCommand.java
+++ src/main/java/com/morrisje/converters/IngredientToIngredientCommand.java
@@ -26,7 +26,9 @@ public class IngredientToIngredientCommand implements Converter<Ingredient, Ingr
 
         IngredientCommand ingredientCommand = new IngredientCommand();
         ingredientCommand.setId(ingredient.getId());
-        ingredientCommand.setRecipeId(ingredient.getRecipe().getId());
+        if(ingredient.getRecipe() !=null){
+            ingredientCommand.setRecipeId(ingredient.getRecipe().getId());
+        }
         ingredientCommand.setAmount(ingredient.getAmount());
         ingredientCommand.setDescription(ingredient.getDescription());
         ingredientCommand.setUom(uomConverter.convert(ingredient.getUom()));
