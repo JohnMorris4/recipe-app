4124708f5e6503bf0fd55c42da4747393e12cf29
johnmorris4 <johnmorris@morrisje.com>
2019-01-14 05:37:32 -0500

    Fixed the converter

--- src/main/java/com/morrisje/converters/IngredientToIngredientCommand.java
+++ src/main/java/com/morrisje/converters/IngredientToIngredientCommand.java
@@ -26,6 +26,7 @@ public class IngredientToIngredientCommand implements Converter<Ingredient, Ingr
 
         IngredientCommand ingredientCommand = new IngredientCommand();
         ingredientCommand.setId(ingredient.getId());
+        ingredientCommand.setRecipeId(ingredient.getRecipe().getId());
         ingredientCommand.setAmount(ingredient.getAmount());
         ingredientCommand.setDescription(ingredient.getDescription());
         ingredientCommand.setUom(uomConverter.convert(ingredient.getUom()));
