9ebbda47a1b662d1c224e8d93408b24537f9eef8
johnmorris4 <johnmorris@morrisje.com>
2019-01-16 04:01:13 -0500

    Created the Update Action on the Controller and Impl classes. Added button on list.html and setup test case.

--- src/main/java/com/morrisje/controllers/IngredientController.java
+++ src/main/java/com/morrisje/controllers/IngredientController.java
@@ -1,6 +1,8 @@
 package com.morrisje.controllers;
 
 import com.morrisje.commands.IngredientCommand;
+import com.morrisje.commands.RecipeCommand;
+import com.morrisje.commands.UnitOfMeasureCommand;
 import com.morrisje.services.IngredientService;
 import com.morrisje.services.RecipeService;
 import com.morrisje.services.UnitOfMeasureService;
@@ -40,6 +42,27 @@ public class IngredientController {
                                        @PathVariable String id, Model model){
         model.addAttribute("ingredient", ingredientService.findByRecipeIdAndIngredientId(Long.valueOf(recipeId), Long.valueOf(id)));
         return "recipe/ingredient/show";
+    }
+
+    @GetMapping
+    @RequestMapping("recipe/{recipeId}/ingredient/new")
+    public String newIngredient(@PathVariable String recipeId, Model model){
+
+        //make sure we have a good id value
+        RecipeCommand recipeCommand = recipeService.findCommandById(Long.valueOf(recipeId));
+        //todo raise exception if null
+
+        //need to return back parent id for hidden form property
+        IngredientCommand ingredientCommand = new IngredientCommand();
+        ingredientCommand.setRecipeId(Long.valueOf(recipeId));
+        model.addAttribute("ingredient", ingredientCommand);
+
+        //init uom
+        ingredientCommand.setUom(new UnitOfMeasureCommand());
+
+        model.addAttribute("uomList",  unitOfMeasureService.listAllUoms());
+
+        return "recipe/ingredient/ingredientform";
     }
 
     @GetMapping
--- src/main/java/com/morrisje/domain/Ingredient.java
+++ src/main/java/com/morrisje/domain/Ingredient.java
@@ -1,13 +1,15 @@
 package com.morrisje.domain;
 
-import lombok.Data;
 import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.Setter;
 
 import javax.persistence.*;
 import java.math.BigDecimal;
 
 
-@Data
+@Getter
+@Setter
 @EqualsAndHashCode(exclude = { "recipe"})
 @Entity
 public class Ingredient {
