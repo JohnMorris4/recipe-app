e69e7bade811ed0439643c40bd22e3c179d93a60
johnmorris4 <johnmorris@morrisje.com>
2019-01-17 04:04:35 -0500

    Setup initial uploading of an image.

---
+++ src/main/java/com/morrisje/services/ImageServiceImpl.java
@@ -0,0 +1,19 @@
+package com.morrisje.services;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+/**
+ * Created by jt on 7/3/17.
+ */
+@Slf4j
+@Service
+public class ImageServiceImpl implements ImageService {
+    @Override
+    public void saveImageFile(Long recipeId, MultipartFile file) {
+
+        log.debug("Received a file");
+
+    }
+}
